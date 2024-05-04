072401b6110b6b3f5b5bfbc650010cd7385d6425
RomeshKTripathi <romeshktripathi@gmail.com>
2024-05-04 08:49:55 +0530

    Minor changes

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import { Schema, model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // third party url of video file.
+      required: [true, "Video file is required"],
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const video = model("video", videoSchema);
