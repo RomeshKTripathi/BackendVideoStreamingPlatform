306ea220a5a1d75a5666f858b52c096008677886
RomeshKTripathi <romeshktripathi@gmail.com>
2024-05-22 16:05:08 +0530

    Controller start

--- src/app.js
+++ src/app.js
@@ -21,4 +21,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+// controllers
+import { registerUser } from "./controllers/user.controller.js";
 export { app };
