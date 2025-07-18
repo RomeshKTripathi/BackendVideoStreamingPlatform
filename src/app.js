c8261533f9891d9b8f0e65c553478d6b0fc8f4af
Romesh Tripathi <romeshktripathi@gmail.com>
2025-07-18 21:16:08 +0530

    Comment Main Route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import { GlobalErrorHandler } from "./middlewares/errorHandler.middleware.js";
+import { ApiError } from "./utils/ApiError.js";
+import commentRouter from "./routes/comment.routes.js";
 const app = express();
 
 app.use(
@@ -21,14 +26,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 // routes
-import userRouter from "./routes/user.routes.js";
-import videoRouter from "./routes/video.routes.js";
-import { GlobalErrorHandler } from "./middlewares/errorHandler.middleware.js";
-import { ApiError } from "./utils/ApiError.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
 app.use("*", (req, res, next) => {
   const err = new ApiError(
     404,
